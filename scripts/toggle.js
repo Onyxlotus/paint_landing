7ff62df0d0dd1354f4b26389a4d406177268e856
Onyxlotus <onyxlotus@yandex.ru>
2025-05-14 22:54:45 +0300

    section 6 mob

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,12 +1,12 @@
 function toggleCard(header) {
-    const content = header.querySelector('.toggleText');
-    const icon = header.querySelector('.toggleIcon');
-    const isOpen = content.classList.toggle('open');
-    const card = header;
+  const content = header.querySelector('.toggleText');
+  const icon = header.querySelector('.toggleIcon');
+  const isOpen = content.classList.toggle('open');
+  const card = header;
 
-    // Меняем src изображения
-    icon.src = isOpen ? './images/minus.svg' : './images/plus.svg';
-      // Плавная анимация через max-height (если хочешь фиксированную высоту — можно вернуть height)
-    card.style.padding = isOpen ? '66px 60px 66px 90px' : '40px 60px 44px 90px';
-    card.style.maxHeight = isOpen ? '272px' : '140px';
-  }
+  // Меняем иконку
+  icon.src = isOpen ? './images/minus.svg' : './images/plus.svg';
+
+  // Добавляем/удаляем класс для управления стилями
+  card.classList.toggle('bigWhiteCard--open', isOpen);
+}
